407106febacba66acc4c3252d60983d35d516df1
Prasad wakchoure <55613046+prasad323122@users.noreply.github.com>
2020-06-13 12:56:03 +0530

    Add files via upload

---
+++ DeclaringVariables.scala
@@ -0,0 +1,29 @@
+object first extends App {
+  println("**********************************")
+
+
+  //Printing a value i Scala
+  println("Hello Avengers")
+
+  //declaring a variable
+  //var: values can be changed
+  var x = 3
+  println(x)
+  x = 3000
+  println(x) //no error
+  //val : value is fixed
+
+  val name = "Tony Stark"
+  println(name)
+
+  //name="ABC"
+  //println(name) //gives reassignment error
+
+  //assigning data type
+
+  val surname: String = "Stark"
+  println(surname)
+  var phnno: Int = 34876528
+  println(phnno)
+
+}
