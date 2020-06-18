98a7e5bdc53728ae742855fed1600fa00e4cf20d
Prasad wakchoure <55613046+prasad323122@users.noreply.github.com>
2020-06-18 21:41:20 +0530

    Add files via upload

---
+++ Datatypes_functions_operators.scala
@@ -0,0 +1,61 @@
+  //*******Data-Types********
+
+    val short1: Short = 20
+    val int1: Int = 30
+    val long1: Long = 3764875
+    val float1: Float = 3.4f
+    val float2: Float = 1e30f
+    val boolean1: Boolean=true;
+    val boolean2: Boolean=false;
+    val char1: Char= 'A'
+    val string1: String="AKA Iron Man"
+    val string2: String=
+        s" I am Tony Stark ${string1} ."
+
+    println(short1)
+    println(int1)
+    println(long1)
+    println(float1)
+    println(float2)
+    println(boolean1,boolean2)
+    println(char1)
+    println(string1)
+    println(string2)
+
+
+
+    //*******Functions of Data-Types********
+
+    println(int1.isValidInt)
+    println(float1.isValidInt)
+
+    1+2
+    println(1+2)
+
+    println(string1.length)
+    println(string1.concat(" The best avenger"))
+    println(string1.indexOf("Iron"))
+    println(string2.toUpperCase)
+    println(string1.substring(6,12))
+println()
+    println()
+    println()
+    println("*************************************")
+    string1 indexOf "Iron"
+
+    val mainstr = "hi i am here to say hello, so hello Evryone"
+
+    println("hello".r.findAllMatchIn(mainstr).length)
+
+//***************OPERATORS******************
+
+//airthmetic: + - * / %
+//Relational: == != > < >= <=
+//Logical: && || !
+//Assignment: += -= *= /= %=
+
+
+
+
+
+
